25b8826253aa73221821d410cf12e63248ff338a
Michael Abel <info@abel-ikt.de>
2024-02-02 21:53:34 +0100

    Add basic functionality for sleep

--- cyclictest-rs/src/lib.rs
+++ cyclictest-rs/src/lib.rs
@@ -1,6 +1,6 @@
 use std::error::Error;
 use std::thread;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
 use clap::Parser;
 
@@ -19,11 +19,25 @@ struct Args {
 
 fn sample_sleep_with_duration(samples: u32, wait_time_ns: u32) -> Result<(), Box<dyn Error>> {
     let sleep_time = Duration::from_nanos(wait_time_ns.into());
-
+    let mut diff: Duration;
+    let mut accumulator: Duration = Duration::new(0, 0);
+    let mut max_diff: Duration = Duration::new(0, 0);
     for _s in 0..samples {
+        let start = Instant::now();
         thread::sleep(sleep_time);
+        let end = Instant::now();
+        diff = end - start;
+        accumulator += diff;
+        if diff > max_diff {
+            max_diff = diff;
+        }
     }
-
+    let average_latency = accumulator / samples - sleep_time;
+    let max_latency = max_diff - sleep_time;
+    println!(
+        "Average Latency {:?} Maximal Latency {:?}",
+        average_latency, max_latency
+    );
     Ok(())
 }
 
@@ -42,7 +56,7 @@ fn sample_clock_nanosleep_with_duration(
 
 pub fn run_with_sleep() -> Result<(), Box<dyn Error>> {
     for _i in 0..10 {
-        sample_sleep_with_duration(1000, 1000)?;
+        sample_sleep_with_duration(1000, 1_000_000)?;
     }
     Ok(())
 }
