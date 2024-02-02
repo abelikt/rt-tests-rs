c4b9b5e65ecedd431c8f53acb17a14eee86a9bf2
Michael Abel <info@abel-ikt.de>
2024-02-02 22:49:45 +0100

    Add clock_nanosleep version

--- cyclictest-rs/src/lib.rs
+++ cyclictest-rs/src/lib.rs
@@ -4,6 +4,16 @@ use std::time::{Duration, Instant};
 
 use clap::Parser;
 
+/*
+
+libc:
+https://docs.rs/libc/
+https://linux.die.net/man/2/clock_nanosleep
+https://manpages.debian.org/bookworm/manpages-dev/clock_nanosleep.2.en.html
+
+*/
+
+
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
@@ -15,6 +25,29 @@ struct Args {
 
     #[arg(long, default_value_t = false)]
     nanosleepgettime: bool,
+}
+
+fn sleep_clock_nanosleep() {
+    //let clockid : libc::clockid_t = libc::CLOCK_REALTIME;
+    let clockid: libc::clockid_t = libc::CLOCK_MONOTONIC;
+
+    let flags: libc::c_int = libc::CLOCK_REALTIME; // 1: libc::TIMER_ABSTIME
+    let request = libc::timespec {
+        tv_sec: 0,
+        tv_nsec: 1_000_000,
+    };
+    let mut remain = libc::timespec {
+        tv_sec: 0,
+        tv_nsec: 0,
+    };
+    let premain: *mut libc::timespec = &mut remain;
+    let ret;
+    unsafe {
+        ret = libc::clock_nanosleep(clockid, flags, &request, premain);
+    }
+    if ret != 0 {
+        println!("clock_nanosleep fails");
+    }
 }
 
 fn sample_sleep_with_duration(samples: u32, wait_time_ns: u32) -> Result<(), Box<dyn Error>> {
@@ -46,11 +79,25 @@ fn sample_clock_nanosleep_with_duration(
     wait_time_ns: u32,
 ) -> Result<(), Box<dyn Error>> {
     let sleep_time = Duration::from_nanos(wait_time_ns.into());
-
+    let mut diff: Duration;
+    let mut accumulator: Duration = Duration::new(0, 0);
+    let mut max_diff: Duration = Duration::new(0, 0);
     for _s in 0..samples {
-        thread::sleep(sleep_time);
+        let start = Instant::now();
+        sleep_clock_nanosleep();
+        let end = Instant::now();
+        diff = end - start;
+        accumulator += diff;
+        if diff > max_diff {
+            max_diff = diff;
+        }
     }
-
+    let average_latency = accumulator / samples - sleep_time;
+    let max_latency = max_diff - sleep_time;
+    println!(
+        "Average Latency {:?} Maximal Latency {:?}",
+        average_latency, max_latency
+    );
     Ok(())
 }
 
@@ -63,7 +110,7 @@ pub fn run_with_sleep() -> Result<(), Box<dyn Error>> {
 
 pub fn run_with_nanosleep() -> Result<(), Box<dyn Error>> {
     for _i in 0..10 {
-        sample_clock_nanosleep_with_duration(1000, 1000)?;
+        sample_clock_nanosleep_with_duration(1000, 1_000_000)?;
     }
     Ok(())
 }
