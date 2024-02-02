3acdc576dc75165041dbaa9671dba07f6b015826
Michael Abel <info@abel-ikt.de>
2024-02-02 21:15:54 +0100

    Add function for simple sleep

--- cyclictest-rs/src/lib.rs
+++ cyclictest-rs/src/lib.rs
@@ -8,10 +8,23 @@ use clap::Parser;
 #[command(author, version, about, long_about = None)]
 struct Args {
     #[arg(long, default_value_t = false)]
+    sleep: bool,
+
+    #[arg(long, default_value_t = false)]
     nanosleep: bool,
 
     #[arg(long, default_value_t = false)]
     nanosleepgettime: bool,
+}
+
+fn sample_sleep_with_duration(samples: u32, wait_time_ns: u32) -> Result<(), Box<dyn Error>> {
+    let sleep_time = Duration::from_nanos(wait_time_ns.into());
+
+    for _s in 0..samples {
+        thread::sleep(sleep_time);
+    }
+
+    Ok(())
 }
 
 fn sample_clock_nanosleep_with_duration(
@@ -24,6 +37,13 @@ fn sample_clock_nanosleep_with_duration(
         thread::sleep(sleep_time);
     }
 
+    Ok(())
+}
+
+pub fn run_with_sleep() -> Result<(), Box<dyn Error>> {
+    for _i in 0..10 {
+        sample_sleep_with_duration(1000, 1000)?;
+    }
     Ok(())
 }
 
@@ -41,6 +61,11 @@ pub fn run_with_nanosleep_gettime() -> Result<(), Box<dyn Error>> {
 pub fn cyclictest_main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
 
+    if args.sleep {
+        println!("simple sleep");
+        run_with_sleep()?;
+    }
+
     if args.nanosleep {
         println!("clock_nanosleep");
         run_with_nanosleep()?;
@@ -56,9 +81,22 @@ pub fn cyclictest_main() -> Result<(), Box<dyn Error>> {
 
 #[cfg(test)]
 mod test {
+    use super::*;
 
     #[test]
     pub fn test1() {
         assert_eq!(1, 1);
+    }
+
+    #[test]
+    fn test_sample_sleep_with_duration() -> Result<(), Box<dyn Error>> {
+        sample_sleep_with_duration(1, 1)?;
+        Ok(())
+    }
+
+    #[test]
+    fn test_sample_clock_nanosleep_with_duration() -> Result<(), Box<dyn Error>> {
+        sample_clock_nanosleep_with_duration(1, 1)?;
+        Ok(())
     }
 }
