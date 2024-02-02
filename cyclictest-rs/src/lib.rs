5a3a03588dbe622730952743db868830686ac216
Michael Abel <info@abel-ikt.de>
2024-02-02 23:41:36 +0100

    Add get and setscheduler

--- cyclictest-rs/src/lib.rs
+++ cyclictest-rs/src/lib.rs
@@ -1,4 +1,5 @@
 use std::error::Error;
+use std::mem;
 use std::thread;
 use std::time::{Duration, Instant};
 
@@ -11,8 +12,19 @@ https://docs.rs/libc/
 https://linux.die.net/man/2/clock_nanosleep
 https://manpages.debian.org/bookworm/manpages-dev/clock_nanosleep.2.en.html
 
-*/
+Run With:
 
+    cargo build && sudo target/debug/cyclictest-rs  --nanosleep
+    cargo build --release && sudo target/release/cyclictest-rs  --nanosleep
+
+Observer real-time prio with:
+
+    ps  -m -C cyclictest-rs -o pid,pri,rtprio,uid,cputime,cmd
+
+    unclear why there is no prio displayed in ps
+    for 6.1.0-17-rt-amd64
+
+*/
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -25,6 +37,44 @@ struct Args {
 
     #[arg(long, default_value_t = false)]
     nanosleepgettime: bool,
+}
+
+fn getscheduler() {
+    let ret;
+    unsafe {
+        ret = libc::sched_getscheduler(0);
+    }
+    println!("Getscheduler {}", ret);
+}
+
+fn setscheduler() -> Result<(), Box<dyn Error>> {
+    // https://linux.die.net/man/2/sched_setscheduler
+    // https://crates.io/crates/scheduler
+    // https://docs.rs/scheduler/0.1.3/scheduler/
+    // https://github.com/terminalcloud/rust-scheduler
+
+    // https://docs.rs/libc/0.2.153/libc/fn.sched_setscheduler.html
+
+    getscheduler();
+
+    let prio = 99;
+    let pid: libc::c_int = 0;
+    let policy: libc::c_int = libc::SCHED_FIFO;
+    let params = libc::sched_param {
+        sched_priority: prio,
+    };
+    let ret;
+    unsafe {
+        ret = libc::sched_setscheduler(pid, policy, &params);
+    }
+
+    if ret != 0 {
+        println!("sched_setscheduler fails");
+    }
+
+    getscheduler();
+
+    Ok(())
 }
 
 fn sleep_clock_nanosleep() {
@@ -109,6 +159,8 @@ pub fn run_with_sleep() -> Result<(), Box<dyn Error>> {
 }
 
 pub fn run_with_nanosleep() -> Result<(), Box<dyn Error>> {
+    setscheduler()?;
+
     for _i in 0..10 {
         sample_clock_nanosleep_with_duration(1000, 1_000_000)?;
     }
